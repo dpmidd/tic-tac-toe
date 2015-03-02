433ef1796777f7163df7d369731ab4c40d5af682
Dmidd87 <dmidd87@gmail.com>
2015-03-02 15:22:46 -0700

    Displays an o on click for each grid

--- app.js
+++ app.js
@@ -2,6 +2,30 @@
 $(document).ready(function() {
 
   $('.cell1').on('click', function() {
-    alert('hi');
+    $('.hiddeno1').removeClass('hiddeno1').addClass('o1');
+  });
+  $('.cell2').on('click', function() {
+    $('.hiddeno2').removeClass('hiddeno2').addClass('o2');
+  });
+  $('.cell3').on('click', function() {
+    $('.hiddeno3').removeClass('hiddeno3').addClass('o3');
+  });
+  $('.cell4').on('click', function() {
+    $('.hiddeno4').removeClass('hiddeno4').addClass('o4');
+  });
+  $('.cell5').on('click', function() {
+    $('.hiddeno5').removeClass('hiddeno5').addClass('o5');
+  });
+  $('.cell6').on('click', function() {
+    $('.hiddeno6').removeClass('hiddeno6').addClass('o6');
+  });
+  $('.cell7').on('click', function() {
+    $('.hiddeno7').removeClass('hiddeno7').addClass('o7');
+  });
+  $('.cell8').on('click', function() {
+    $('.hiddeno8').removeClass('hiddeno8').addClass('o8');
+  });
+  $('.cell9').on('click', function() {
+    $('.hiddeno9').removeClass('hiddeno9').addClass('o9');
   });
 });
